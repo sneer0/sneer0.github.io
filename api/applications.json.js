d4bc1120e3c34bb879ff19df6b41b1f5d9e40463
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-11-08 03:43:17 +0800

    Add files via upload

---
+++ api/applications.json.js
@@ -0,0 +1,62 @@
+let content = [
+    {
+        "name": "APP下载",
+        "id": "android_applications",
+        "items": [
+            {
+                "name": "星火",
+                "id": "xh",
+                "summary": "轻度浏览，不沉迷！",
+                "image": "/images/graphs/aidelua.webp",
+                "src": "https://sneer0.github.io/xh",
+                "icon": "home-outline",
+                "url-type": "去下载"
+            },
+            {
+                "name": "留声机",
+                "id": "lsj",
+                "summary": "留住消逝的声音，让热爱音乐的人相遇~",
+                "image": "/images/graphs/aidelua.webp",
+                "src": "https://sneer0.github.io/lsj",
+                "icon": "home-outline",
+                "url-type": "去下载"
+            },
+            {
+                "name": "网购助手",
+                "id": "wgzs",
+                "summary": "低价捡漏，薅羊毛！",
+                "image": "/images/graphs/aidelua.webp",
+                "src": "http://ocjxs.yhzu.cn",
+                "icon": "home-outline",
+                "url-type": "去购物"
+            },
+        ]
+    },
+
+    {
+        "name": "歌单推荐",
+        "id": "pwa_applications",
+        "items": [
+            {
+                "name": "测试",
+                "id": "baiduhome_lite",
+                "summary": "测试测试测试测试",
+                "image": "/images/graphs/baidu_lite.webp",
+                "src": "/baiduhomelite/",
+                "icon": "open-in-app",
+                "url-type": "应用"
+            },
+            {
+                "name": "测试",
+                "id": "deskclock_lite",
+                "summary": "测试测试测试测试",
+                "image": "/images/graphs/deskclock_lite.webp",
+                "src": "/deskclocklite/",
+                "icon": "open-in-app",
+                "url-type": "应用"
+            }
+        ]
+    },
+
+ 
+]
