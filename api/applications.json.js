16c7a78841d4715d21277b01238800b938c4864b
嘲讽 <112706806+sneer0@users.noreply.github.com>
2023-11-09 00:35:09 +0800

    Add files via upload

--- api/applications.json.js
+++ api/applications.json.js
@@ -7,28 +7,28 @@ let content = [
                 "name": "星火",
                 "id": "xh",
                 "summary": "轻度浏览，不沉迷！",
-                "image": "/img/111.png",
+                "image": "/img/logo-xh.png",
                 "src": "https://sneer0.github.io/xh",
                 "icon": "home-outline",
-                "url-type": "去下载"
+                "url-type": "去下载",
             },
             {
                 "name": "留声机",
                 "id": "lsj",
-                "summary": "留住消逝的声音。",
-                "image": "/images/graphs/aidelua.webp",
+                "summary": "留住消逝的声音",
+                "image": "/img/logo-lsj.png",
                 "src": "https://sneer0.github.io/lsj",
                 "icon": "home-outline",
-                "url-type": "去下载"
+                "url-type": "去下载",
             },
             {
                 "name": "网购助手",
                 "id": "wgzs",
                 "summary": "低价捡漏，薅羊毛！",
-                "image": "/images/graphs/aidelua.webp",
+                "image": "/img/logo-gw.png",
                 "src": "http://ocjxs.yhzu.cn",
                 "icon": "home-outline",
-                "url-type": "去购物"
+                "url-type": "去购物",
             },
         ]
     },
@@ -38,23 +38,86 @@ let content = [
         "id": "pwa_applications",
         "items": [
             {
-                "name": "测试",
-                "id": "baiduhome_lite",
-                "summary": "测试测试测试测试",
-                "image": "/images/graphs/baidu_lite.webp",
-                "src": "/baiduhomelite/",
+                "name": "养生音乐｜调理身体",
+                "id": "deskclock_lite",
+                "summary": "古法养生音药，心肝脾肺肾一站式调理",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=8681335851",
                 "icon": "open-in-app",
-                "url-type": "应用"
+                "url-type": "去听歌",
             },
             {
-                "name": "测试",
-                "id": "deskclock_lite",
-                "summary": "测试测试测试测试",
-                "image": "/images/graphs/deskclock_lite.webp",
-                "src": "/deskclocklite/",
+                "name": "莫扎特K448",
+                "id": "baiduhome_lite",
+                "summary": "能短期提升智商的钢琴曲",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=6947213494",
                 "icon": "open-in-app",
-                "url-type": "应用"
-            }
+                "url-type": "去听歌",
+            },
+            {
+                "name": "巴赫精选｜抗焦虑",
+                "id": "baiduhome_lite",
+                "summary": "缓解焦虑，舒缓心情",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=7768815745",
+                "icon": "open-in-app",
+                "url-type": "去听歌"
+            },
+            {
+                "name": "肖邦全集｜改变浮躁",
+                "id": "baiduhome_lite",
+                "summary": "烦躁的时候听",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=7768547696",
+                "icon": "open-in-app",
+                "url-type": "去听歌"
+            },
+            {
+                "name": "舒伯特｜提升学习效率",
+                "id": "baiduhome_lite",
+                "summary": "适合学习时候听的钢琴曲",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=7768786092",
+                "icon": "open-in-app",
+                "url-type": "去听歌"
+            },
+            {
+                "name": "风吹得我们想跳舞",
+                "id": "baiduhome_lite",
+                "summary": "在散漫的节奏中被音乐灌醉",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=6633413569",
+                "icon": "open-in-app",
+                "url-type": "去听歌"
+            },
+            {
+                "name": "18岁的老年生活",
+                "id": "baiduhome_lite",
+                "summary": "21世纪疲惫的年轻人",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=6633843527",
+                "icon": "open-in-app",
+                "url-type": "去听歌"
+            },
+            {
+                "name": "催眠曲｜5分钟入睡",
+                "id": "baiduhome_lite",
+                "summary": "在散漫的节奏中被音乐灌醉",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=5277751159",
+                "icon": "open-in-app",
+                "url-type": "去听歌"
+            },
+            {
+                "name": "我的26岁女房客",
+                "id": "baiduhome_lite",
+                "summary": "一本书，一个歌单。",
+                "image": "/img/logo-xh.png",
+                "src": "https://music.163.com/#/playlist?id=5078965624",
+                "icon": "open-in-app",
+                "url-type": "去听歌"
+            },
         ]
     },
 
